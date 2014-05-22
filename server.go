c45231874d99ebf8db2421d56193b9b55711aa21
Ryan Cheu <ryancheu@mit.edu>
2014-05-22 15:56:05 -0400

    Added command setting/viewing

--- server.go
+++ server.go
@@ -18,6 +18,8 @@ type Coordinate struct {
 var coords map[int]Coordinate
 var curIndex = 0
 
+var curCommand string
+
 func coordinateAddHandler(rw http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	lat := query.Get("lat")
@@ -34,7 +36,7 @@ func coordinateListHandler(rw http.ResponseWriter, r *http.Request) {
 	//This is some bad code that transfers the map into a fixed length array
 	//This should really be refactored to use the go equivalent of vector/arraylist
 	//so this step isn't needed
-	var coordArray []Coordinate = make([]Coordinate, len(coords)) 
+	var coordArray []Coordinate = make([]Coordinate, len(coords))
 	for i := 0; i < len(coords); i++ {
 		coordArray[len(coords)-1-i] = coords[i]
 	}
@@ -54,10 +56,24 @@ func coordinateClearHandler(rw http.ResponseWriter, r *http.Request) {
 	coords = make(map[int]Coordinate)
 }
 
+func commandSetHandler(rw http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	command := query.Get("command")
+	curCommand = command
+}
+
+func commandViewHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/html")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprint(rw, curCommand)
+}
+
 func main() {
 	coords = make(map[int]Coordinate)
 	http.HandleFunc("/coordinate/new", coordinateAddHandler)
 	http.HandleFunc("/coordinate/list", coordinateListHandler)
 	http.HandleFunc("/coordinate/clear", coordinateClearHandler)
+	http.HandleFunc("/command/set", commandSetHandler)
+	http.HandleFunc("/command/view", commandViewHandler)
 	http.ListenAndServe(":"+strconv.Itoa(8080), nil)
 }
