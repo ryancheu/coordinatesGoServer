2836acd166aa20ee95a41d59f0c1518c5859ccd4
Ryan Cheu <ryancheu@mit.edu>
2014-05-21 21:49:31 -0400

    more code cleanup

--- server.go
+++ server.go
@@ -7,41 +7,14 @@ import (
 	"strconv"
 )
 
-type QualityType struct {
-	Play    string
-	Token   string
-	Connect string `"json:"connect"`
-	Bitrate float64
-}
-
 type Coordinate struct {
 	Lat  string
 	Long string
 }
 
-//Snippet for json responses taken from
-//http://nesv.blogspot.com/2012/09/super-easy-json-http-responses-in-go.html
-type Response map[string]interface{}
-
-func (r Response) String() (s string) {
-	b, err := json.Marshal(r)
-	if err != nil {
-		s = ""
-		return
-	}
-	s = string(b)
-	return
-}
-
-const startPort = 6000
-const maxPorts = 1000
-const queryPort = 8080
-const bitrate = 128
-const refreshPeriod = 600 //In minutes
-
 //This should really use a list but I don't know how to do that yet in Golang
 var coords map[int]Coordinate
-var curIndex = startPort
+var curIndex = 0
 
 func coordinateAddHandler(rw http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
@@ -88,5 +61,5 @@ func main() {
 	http.HandleFunc("/coordinate/new", coordinateAddHandler)
 	http.HandleFunc("/coordinate/list", coordinateListHandler)
 	http.HandleFunc("/coordinate/clear", coordinateClearHandler)
-	http.ListenAndServe(":"+strconv.Itoa(queryPort), nil)
+	http.ListenAndServe(":"+strconv.Itoa(8080), nil)
 }
